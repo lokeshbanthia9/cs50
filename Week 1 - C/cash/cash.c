b597d62c9fc6858c4eb4ec21c61ea784f28cc938
lokeshbanthia9 <banthialokesh@gmail.com>
2025-01-25 01:28:42 +0530

    cash change coins

---
+++ Week 1 - C/cash/cash.c
@@ -0,0 +1,54 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+const int change_list_desc[] = {25, 10, 5, 1};
+const int number_of_change = 4;
+
+int get_amount();
+int calculate_minimum_coins(int amount);
+
+int main(void)
+{
+  int amount = get_amount();
+  int coins = calculate_minimum_coins(amount);
+  printf("%d\n", coins);
+}
+
+
+// Get the change owed amount
+int get_amount()
+{
+  int n;
+  char ch;
+
+  while (true)
+  {
+    printf("Change owed: ");
+
+    if (scanf("%d", &n) == 1 && n >= 0)
+    {
+      return n;
+    }
+    else
+    {
+      while ((ch = getchar()) != '\n' && ch != EOF);
+    }
+  }
+}
+
+// Calculate minimum number of coins required for the owed amount
+int calculate_minimum_coins(int amount)
+{
+  int coins = 0;
+
+  for (int i = 0; i < number_of_change; i++)
+  {
+    while (amount > 0 && change_list_desc[i] <= amount)
+    {
+      amount -= change_list_desc[i];
+      coins++;
+    }
+  }
+
+  return coins;
+}
